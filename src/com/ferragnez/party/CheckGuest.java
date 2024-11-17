fc69c268f748b73a80f57768e5823d18e4dd3367
giovanniocchipinti <giovanniocchipinti2699@gmail.com>
2024-11-17 19:24:52 +0100

    feat: created new scanner to ask an user input

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,8 +1,21 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        //* Guests list invited to party
         String[] guests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeili"};
-    
+        //* Creo uno scanner */
+        Scanner input = new Scanner(System.in);
+        //*Chiedo all'utente di inserire il suo nome in lista */
+        System.out.println("Inserisci il tuo nome:");
+        String guestName = input.nextLine();
+        input.close();
+        //*Creo un ciclo for per verificare se il nome inserito si trova nella guests list */
+
+
     }
+        
+
 }
